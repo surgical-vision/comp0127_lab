905d2f426068dc0adfbbfd0f60288cc033086e1c
kpach <krittin.pachtrachai.13@ucl.ac.uk>
2020-10-05 15:22:10 +0100

    Add comments

--- lab01/youbot_traj_example/src/youbot_traj_example_node.cpp
+++ lab01/youbot_traj_example/src/youbot_traj_example_node.cpp
@@ -2,6 +2,7 @@
 #include <trajectory_msgs/JointTrajectory.h>
 #include <std_msgs/Float64.h>
 
+//Manually pre-define joint positions
 double joint1[10] = {1.7768, 0.2057, 2.8906, 3.3794, 2.9092, 2.3643, 1.2801, 2.2591, 1.7753, 0.9860};
 double joint2[10] = {1.7252, 0.0778, 0.6767, 0.1128, 0.2373, 2.0121, 1.6978, 0.7748, 2.3218, 0.0842};
 double joint3[10] = {-1.2207, -1.9312, -1.8746, -1.0249, -0.9458, -1.4789, -1.2553, -1.2713, -1.5904, -1.8198};
@@ -9,8 +10,9 @@ double joint4[10] = {0.9635, 1.3726, 1.2867, 0.5676, 0.4154, 1.7396, 1.3501, 1.1
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "trajectory_generator");
-
+    
     ros::NodeHandle nh;
+    
     ros::Publisher j1_pub = nh.advertise<std_msgs::Float64>(
             "/EffortJointInterface_J1_controller/command", 10);
     ros::Publisher j2_pub = nh.advertise<std_msgs::Float64>(
@@ -21,14 +23,14 @@ int main(int argc, char **argv) {
             "/EffortJointInterface_J4_controller/command", 10);
 
     int t = 0;
-
+    //Initialise the message variables.
     std_msgs::Float64 msg1;
     std_msgs::Float64 msg2;
     std_msgs::Float64 msg3;
     std_msgs::Float64 msg4;
 
     while (nh.ok()) {
-
+        //Manually moving the arm by sending the joint positions to each topic.
         msg1.data = joint1[t];
         msg2.data = joint2[t];
         msg3.data = joint3[t];
