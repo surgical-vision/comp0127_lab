e486d205d316b6e774080edaca19f148a20e1b2b
emdimitrakakis <e.dimitrakakis@ucl.ac.uk>
2021-10-29 15:04:35 +0100

    Updated lab 04 materials.

--- lab04/lab04_example/src/lab04_example.py
+++ lab04/lab04_example/src/lab04_example.py
@@ -5,13 +5,12 @@ from math import pi
 import numpy as np
 from numpy import arctan2
 from geometry_msgs.msg import Quaternion
-#TODO
 #import the message type that holds data describing robot joint angles
-
+from sensor_msgs.msg import JointState
 #import the class that publishes coordinate frame transform information
-
+from tf2_ros import TransformBroadcaster
 #import the message type that expresses a transformt from one coordinate frame to another
-
+from geometry_msgs.msg import TransformStamped, Quaternion
 
 #The OpenManipulator DH parameters
 #a1 is 0.012 rather than 0 to align with the URDF file
@@ -21,7 +20,7 @@ d = [0.075, 0.0, 0.0, 0.0]
 theta = [0.0, -1.385, 1.385, 0.0]
 
 #TODO: #define the frame names
-
+name_link = ['fkine_link_1']
 
 def forward_kinematics(a1, alpha1, d1, theta1):
     #This function returns the hard-coded T01 matrix for joint frame 1. 
@@ -85,29 +84,32 @@ def fkine_wrapper(joint_msg, br):
     for i in range(1):
         
         #for every joint, calculate the forward kinematics -> here hard-coded
-
+        T01 = forward_kinematics(a[0], alpha[0], d[0], theta[0] + joint_msg.position[i])
 
         #Here we define the transform from joint frame 0 to joint frame 1 using the tf2 broadcaster.
         #define the transfrom timestamp
-
+        transform.header.stamp = rospy.Time.now()
         #define the transfrom header frame (parent frame)
-
+        transform.header.frame_id = 'world'
         #define the transfrom child frame
-
+        transform.child_frame_id = name_link[i]
         #populate the transform field. It consists of translation and rotation.
-
+        transform.transform.translation.x = T01[0, 3]
+        transform.transform.translation.y = T01[1, 3]
+        transform.transform.translation.z = T01[2, 3]
+        transform.transform.rotation = rotmat2q(T01)
 
         #broadcast the transform to tf2
-
+        br.sendTransform(transform)
 
 def main():
     rospy.init_node('open_forward_kinematic_node')
 
     ##TODO: Initialise the broadcaster
-
+    br = TransformBroadcaster()
 
     ##TODO: Initialise the subscriber.
-
+    sub = rospy.Subscriber('/joint_states', JointState, fkine_wrapper, br)
 
     rospy.spin()
 
