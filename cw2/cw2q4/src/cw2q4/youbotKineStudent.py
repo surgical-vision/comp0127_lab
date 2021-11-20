b1967b2e8ed53f9c9c66abc68947d8bb9e908824
emdimitrakakis <e.dimitrakakis@ucl.ac.uk>
2021-11-20 09:57:20 +0000

    Updated cw2q4.

--- cw2/cw2q4/src/cw2q4/youbotKineStudent.py
+++ cw2/cw2q4/src/cw2q4/youbotKineStudent.py
@@ -43,7 +43,6 @@ class YoubotKinematicStudent(YoubotKinematicBase):
         T = np.identity(4)
         # Apply offset and polarity to joint readings (found in URDF file)
         joints_readings = [sign * angle for sign, angle in zip(self.youbot_joint_readings_polarity, joints_readings)]
-        # your code starts here ------------------------------
 
         for i in range(up_to_joint):
             A = self.standard_dh(self.dh_params['a'][i],
@@ -52,7 +51,6 @@ class YoubotKinematicStudent(YoubotKinematicBase):
                                  self.dh_params['theta'][i] + joints_readings[i])
             T = T.dot(A)
 
-        # your code ends here -------------------------------
         assert isinstance(T, np.ndarray), "Output wasn't of type ndarray"
         assert T.shape == (4, 4), "Output had wrong dimensions"
         return T
@@ -71,39 +69,8 @@ class YoubotKinematicStudent(YoubotKinematicBase):
         assert len(joint) == 5
 
         # Your code starts here ----------------------------
-        T1 = self.forward_kinematics(joint, 1)
-        T2 = self.forward_kinematics(joint, 2)
-        T3 = self.forward_kinematics(joint, 3)
-        T4 = self.forward_kinematics(joint, 4)
-        T5 = self.forward_kinematics(joint, 5)
 
-        # Bit of trick here to match Jacobian of KDL with z0 being [0, 0, -1] since that is how its define in the URDF
-        # instead of [0, 0, 1]
-        z0 = np.array([0, 0, -1])
-        z1 = np.array([T1[0, 2], T1[1, 2], T1[2, 2]])
-        z2 = np.array([T2[0, 2], T2[1, 2], T2[2, 2]])
-        z3 = np.array([T3[0, 2], T3[1, 2], T3[2, 2]])
-        z4 = np.array([T4[0, 2], T4[1, 2], T4[2, 2]])
 
-        o0 = np.array([0, 0, 0])
-        o1 = np.array([T1[0, 3], T1[1, 3], T1[2, 3]])
-        o2 = np.array([T2[0, 3], T2[1, 3], T2[2, 3]])
-        o3 = np.array([T3[0, 3], T3[1, 3], T3[2, 3]])
-        o4 = np.array([T4[0, 3], T4[1, 3], T4[2, 3]])
-        Pe = np.array([T5[0, 3], T5[1, 3], T5[2, 3]])
-
-        JP1 = np.cross(z0, np.subtract(Pe, o0))
-        JP2 = np.cross(z1, np.subtract(Pe, o1))
-        JP3 = np.cross(z2, np.subtract(Pe, o2))
-        JP4 = np.cross(z3, np.subtract(Pe, o3))
-        JP5 = np.cross(z4, np.subtract(Pe, o4))
-
-        J1 = np.hstack((JP1, z0))
-        J2 = np.hstack((JP2, z1))
-        J3 = np.hstack((JP3, z2))
-        J4 = np.hstack((JP4, z3))
-        J5 = np.hstack((JP5, z4))
-        jacobian = np.transpose(np.vstack((J1, J2, J3, J4, J5)))
         # Your code ends here ------------------------------
         assert jacobian.shape == (6, 5)
         return jacobian
@@ -123,12 +90,7 @@ class YoubotKinematicStudent(YoubotKinematicBase):
         assert len(joint) == 5
         # Your code starts here ----------------------------
 
-        Jacobian = self.get_jacobian(joint)
-        product = np.dot(np.transpose(Jacobian), Jacobian)
-        if (np.linalg.det(product) < 0.00000001):
-            singularity = True
-        else:
-            singularity = False
+
 
         # Your code ends here ------------------------------
         assert isinstance(singularity, bool)
